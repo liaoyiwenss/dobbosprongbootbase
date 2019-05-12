b5018553b9412bd54909ad8a84d906b29666cbe3
liaoyiwen <953013493@qq.com>
2019-05-12 21:48:34 +0800

    Vo

--- server-provider/src/main/java/cc/mrbird/ProviderApplicaiton.java
+++ server-provider/src/main/java/cc/mrbird/ProviderApplicaiton.java
@@ -1,9 +1,9 @@
 package cc.mrbird;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableDubbo
 @SpringBootApplication
